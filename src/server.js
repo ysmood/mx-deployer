99b5d6bcc143ef9bf09aed5c44a766f83635be12
chenjunyu <842914439@qq.com>
2015-09-28 16:43:01 +0800

    add logs.

--- src/server.js
+++ src/server.js
@@ -8,8 +8,23 @@ let br = kit.require('brush');
 let proxy = kit.require('proxy');
 let { match, select } = proxy;
 let { spawn } = require('child_process');
+let fs = require('fs');
+let path = require('path');
 
 let app = proxy.flow();
+
+var addLog = (info, proc) => {
+    let now = new Date();
+    let logName = path.join(__dirname, '../log/' + 
+        now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate() + '.log');
+    let logStream = fs.createWriteStream(logName, {
+        flags: 'a'
+    });
+    logStream.write("[time:" + now.getTime() + "]\n");
+    logStream.write("[user:" + info.user + "]\n");
+    proc.stdout.pipe(logStream);
+    proc.stderr.pipe(logStream);
+}
 
 export default async (opts) => {
     opts = kit._.defaults(opts, {
@@ -38,6 +53,8 @@ export default async (opts) => {
 
             proc.stdout.pipe($.res);
             proc.stderr.pipe($.res);
+
+            addLog(info, proc);
 
             return kit.end();
         })
