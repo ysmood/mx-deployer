f5d38fb94da5ff1a815c6dc7db2b992f7a8a47ba
chenjunyu <842914439@qq.com>
2015-09-28 17:54:08 +0800

    typo

--- bin/mx-deployer-server.js
+++ bin/mx-deployer-server.js
@@ -7,7 +7,6 @@ var main = require('../lib/server');
 cmder
     .option('--port <str>', 'port [8710]')
     .option('--bin <str>', 'the binary to run deploy [node]')
-    .option('--shell <str>', 'the shell to run the deploy hook [bash]')
 .parse(process.argv);
 
 main(utils.getOpts(cmder))
